2091581c5544e08ee866fce19e6c36834123e82a
Rombus1234 <81769150+krishnppatel158@users.noreply.github.com>
2024-06-10 15:43:49 -0400

    Add files via upload

---
+++ jQuery.js
@@ -0,0 +1,37 @@
+// jQuery.js
+
+$ (document).ready(function(){ // document ready event
+    
+    
+   // Courses tile1
+    $(".tile1").hover(function(){
+        
+        $('.tile-title1').css('color','#FFD700' );
+         
+    });
+    // Courses tile2
+    $(".tile2").hover(function(){
+         
+         $('.tile-title2').css('color','#FFD700' );
+         
+     });
+     
+     // Courses tile3
+     $(".tile3").hover(function(){
+         
+         $('.tile-title3').css('color','#FFD700' );
+         
+     });
+     
+     
+     // Courses tile4
+     $(".tile4").hover(function(){
+         
+         $('.tile-title4').css('color','#FFD700' );
+         
+    });
+     
+     
+     
+     
+});// ending of jQuery
